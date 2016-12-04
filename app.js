b439cc1df2149d6cb85f7d9b14d5a01426a51c8c
John Rodler <john@jrod.io>
2016-12-04 14:15:27 -0800

    cleanup blurred + focused in app.js

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
+// const electron = require('electron');
 const Menubar = require('menubar');
 
 let mb = Menubar({
+  // closable: false,
   // resizable: false,
   alwaysOnTop: true,
   // showDockIcon: true,
-  // preloadWindow: true,
+  preloadWindow: true,
   transparent: true
 });
 
 mb.on('ready', () => {
+  let wc = mb.window.webContents;
+
+  mb.window.on('focus', () => {
+    wc.send('window-focus');
+  });
+
+  mb.on('focus-lost', () => {
+    wc.send('window-blur');
+  });
+
+  mb.tray.on('drop-text', function (e, text) {
+    console.log(text);
+  });
+});
+
+mb.on('create-window', () => {
   // const screen = electron.screen.getPrimaryDisplay().workArea;
   const width = 500;
   const height = 280;
@@ -22,13 +40,4 @@ mb.on('ready', () => {
 
 mb.on('after-create-window', () => {
   mb.window.openDevTools();
-  let wc = mb.window.webContents;
-
-  mb.window.on('focus', () => {
-    wc.send('window-focus');
-  });
-
-  mb.on('focus-lost', () => {
-    wc.send('window-blur');
-  });
 });
