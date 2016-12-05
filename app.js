04b0df12ea807326e0d802761ced3203686d643d
John Rodler <john@jrod.io>
2016-12-05 08:44:41 -0800

    setup parse drop-link-text + load video player logic

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 "use strict";
 
-// const electron = require('electron');
 const Menubar = require('menubar');
 
 let mb = Menubar({
   alwaysOnTop: true,
-  // showDockIcon: true,
   preloadWindow: true,
   transparent: true
+  // tooltip: 'Drop YouTube + Vimeo Links Here'
 });
 
+mb.on('create-window', () => {
+  mb.setOption('y', 25);
+  mb.setOption('width', 500);
+  mb.setOption('height', 280);
+});
 
-mb.on('ready', () => {
+mb.on('after-create-window', () => {
   let wc = mb.window.webContents;
 
-  mb.window.setAspectRatio(16/9, { height: 20, width: 0 });
+  mb.window.setAspectRatio(16/9, { height: 0, width: 0 });
+  mb.window.loadURL(`file://${__dirname}/index.html`);
+  // mb.window.openDevTools();
 
   mb.window.on('focus', () => {
     wc.send('window-focus');
@@ -25,25 +31,7 @@ mb.on('ready', () => {
   });
 
   mb.tray.on('drop-text', function (e, text) {
-    console.log(text);
-  });
-});
-
-
-mb.on('create-window', () => {
-  // const screen = electron.screen.getPrimaryDisplay().workArea;
-  const width = 500;
-  const height = 300;
-  // const x = (screen.width - width) - 3;
-
-  // mb.setOption('x', x);
-  mb.setOption('y', 25);
-  mb.setOption('width', width);
-  mb.setOption('height', height);
-});
-
-
-mb.on('after-create-window', () => {
-  mb.window.openDevTools();
-  mb.window.loadURL(`file://${__dirname}/index.html`);
+    wc.send('dropped-text', text);
+    mb.showWindow();
+  });  
 });
--- index.js
+++ index.js
@@ -1,31 +1,65 @@
-let { ipcRenderer } = require('electron');
+"use strict";
+
+let ipc = require('electron').ipcRenderer;
+let $ = require('jquery');
 
 // events
-ipcRenderer.on('window-blur', () => {
-  document.body.classList.add('isBlurred');
-});
-ipcRenderer.on('window-focus', () => {
-  document.body.classList.remove('isBlurred');
+ipc.on('window-blur', () => {
+  $('body').addClass('isBlurred');
 });
 
-// wrapper
-let container = document.createElement('div');
-container.className = 'container';
+ipc.on('window-focus', () => {
+  $('body').removeClass('isBlurred');
+});
 
-// player
-let video = document.createElement('iframe');
-video.className = 'video';
-video.src = 'http://www.youtube.com/embed/Pi3WGM_iXAQ';
-/* video.width = '600';
-   video.height = '400'; */
-video.setAttribute('frameborder', '0');
-video.setAttribute('allowFullScreen', '');
+ipc.on('dropped-text', (e, text) => {
+  if (/youtu/.test(text) || /vimeo/.test(text)) {
+    let id = /youtu/.test(text) ? getYouTubeId(text) : getVimeoId(text);
+    if (id) {
+      let link = /youtu/.test(text)
+        ? `https://www.youtube.com/embed/${id}?autoplay=1`
+        : `https://player.vimeo.com/video/${id}?autoplay=1`;
+      if ($('#video iframe').length) $('#video iframe').remove();
+      $('#drop-splash').addClass('isHidden');
+      $(`<iframe src="${link}" frameborder="0" scrolling="no" allowFullScreen></iframe>`).appendTo('#video');
+      $('#video').removeClass('isHidden');
+    } else {
+      $('#video').addClass('isHidden');
+      $('#drop-splash span').text('Invalid YouTube / Vimeo Link ID');
+      $('#drop-splash').removeClass('isHidden');
+    }
+  } else {
+    $('#video').addClass('isHidden');
+    $('#drop-splash span').text('Invalid YouTube / Vimeo URL');
+    $('#drop-splash').removeClass('isHidden');
+  }
+});
 
-// drag handle / control bar
-let drag = document.createElement('div');
-drag.className = 'drag-handle';
+// video URL helpers
+function getVideoId(str, prefixes) {
+  var cleaned = str.replace(/^(https?:)?(\/\/)(www\.)?/, '');
+  for(var i = 0; i < prefixes.length; i++) {
+    if (cleaned.indexOf(prefixes[i]) === 0) {
+      let id = cleaned.substr(prefixes[i].length);
+      id = id.match(/[^?]*/i)[0];
+      return id.match(/[^&]*/i)[0];
+    }
+  }
+  return false;
+}
 
-// update
-container.appendChild(video);
-document.body.appendChild(container);
-document.body.appendChild(drag);
+function getYouTubeId(url) {
+  return getVideoId(url, [
+    'youtube.com/watch?v=',
+    'youtu.be/',
+    'youtube.com/embed/'
+  ]);
+}
+
+function getVimeoId(url) {
+  return getVideoId(url, [
+    'vimeo.com/',
+    'player.vimeo.com/video/',
+    'player.vimeo.com/'
+  ]);
+}
