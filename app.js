ebf1dfb19a7ecd3670dbdff10383252b08f6b506
John Rodler <john@jrod.io>
2016-12-05 15:35:49 -0800

    maintain pos/dimensions when loading a new video in an active window

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mb.on('create-window', () => {
 mb.on('after-create-window', () => {
   let wc = mb.window.webContents;
 
-  // mb.window.openDevTools();
+  mb.window.openDevTools();
   mb.window.setMinimumSize(260, 146);
   mb.window.setMaximumSize(960, 540);
   mb.window.setAspectRatio(16/9, { height: 0, width: 0 });
@@ -38,8 +38,18 @@ mb.on('after-create-window', () => {
   });
 
   mb.tray.on('drop-text', (e, text) => {
+    let pos = mb.window.getPosition();
+    if (mb.window.isVisible()) {
+      mb.setOption('x', pos[0]);
+      mb.setOption('y', pos[1]);
+    } else {
+      let trayPos = mb.positioner.calculate('trayCenter', mb.tray.getBounds());
+      mb.setOption('x', trayPos.x);
+      mb.setOption('y', 25);
+      mb.window.setSize(500, 280);
+      mb.showWindow();
+    }
     wc.send('dropped-text', text);
-    mb.showWindow();
   });
 });
 
