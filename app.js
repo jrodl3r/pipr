d8aca6406319b8d9ad42edf55e17fb57f0299386
John Rodler <john@jrod.io>
2016-12-10 08:55:38 -0800

    add window vibrancy / transparency

--- app.js
+++ app.js
@@ -66,6 +66,7 @@ mb.on('after-create-window', () => {
   // mb.window.setMaximumSize(960, 540);
   mb.window.setMinimumSize(260, 146);
   mb.window.setAspectRatio(16/9, { height: 0, width: 0 });
+  mb.window.setVibrancy('ultra-dark');
   mb.window.loadURL(`file://${__dirname}/index.html`);
 
   mb.window.on('focus', () => { wc.send('window-focus'); });
